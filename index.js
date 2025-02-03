4b5e304644a166916b522ca0405d5fbcd0cce3b9
Syeda Fairooz Nawal <syedafairooznawal@gmail.com>
2025-02-04 01:06:01 +0600

    Making changes

--- index.js
+++ index.js
@@ -7,12 +7,7 @@ const port = process.env.PORT || 5000;
 
 //middleware
 
-
-app.use(cors({
-  origin: "*",
-  credentials: true
-
-}))
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -144,5 +139,5 @@ run().catch(console.dir);
 
 
 app.listen(port, () => {
-  console.log(`Coffee server is listening on port ${port}`)
+  // console.log(`Coffee server is listening on port ${port}`)
 })
